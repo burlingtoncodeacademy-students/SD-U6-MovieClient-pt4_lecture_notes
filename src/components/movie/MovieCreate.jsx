05d35cc8fb07a1498894cb4f605276be913cece9
Eric Winebrenner <ejwinebrenner@gmail.com>
2024-01-19 15:28:21 -0500

    updated base notes

---
+++ src/components/movie/MovieCreate.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function MovieCreate() {
+    return (
+        <div>MovieCreate</div>
+    )
+}
+
+export default MovieCreate
---
+++ src/components/movie/MovieIndex.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MovieCreate from './MovieCreate';
+
+function MovieIndex() {
+    return (
+        <>
+            MovieIndex
+            <MovieCreate />
+        </>
+    )
+}
+
+export default MovieIndex
